ef17a3ce7ccefb59ab68976a43da05b975aebb09
Aeolus <aeolus@AeolusdeMacBook-Pro.local>
2017-09-12 15:47:29 +0800

    update

--- class2/settings.py
+++ class2/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'class2'
+    'class2',
+    'company'
+    
 ]
 
 MIDDLEWARE = [
--- company/models.py
+++ company/models.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.db.models import DateTimeField
 from django.db import models
 
 # Create your models here.
 
 class Company(models.Model):
     name = models.CharField(max_length=30)
-    create_time = models.DateTimeField(default=datetime.now)
-    update_time = models.DateTimeField(default=datetime.now)
+    create_time = models.DateTimeField(auto_now=True)
+    update_time = models.DateTimeField(auto_now=True)
     
 class Detail(models.Model):
     name = models.CharField(max_length=30)
-    create_time = models.DateTimeField(default=datetime.now)
+    create_time = models.DateTimeField(auto_now=True)
     account_version = models.CharField(max_length=30)
 
